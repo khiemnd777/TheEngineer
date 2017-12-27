12eb6a7adc71575f899fc705c55688a5658b4827
khiemnd777 <khiemnd777@gmail.com>
2017-12-27 08:56:44 +0700

    dummies python on game

--- Source/Assets/Scripts/DocumentEvents.cs
+++ Source/Assets/Scripts/DocumentEvents.cs
@@ -1,23 +1,30 @@
 using UnityEngine;
+using UnityEngine.EventSystems;
 
-public class DocumentEvents : MonoBehaviour
+public class DocumentEvents : MonoBehaviour, IPointerDownHandler
 {
-    public void DocumentEventClick()
+    // void Update()
+    // {
+    //     if (Input.GetMouseButtonDown(0))
+    //     {
+    //         RaycastHit2D hit = Physics2D.Raycast(Camera.main.ScreenToWorldPoint(Input.mousePosition), Vector2.zero);
+    //         if (hit.collider != null)
+    //         {
+    //             Debug.Log("Target Position: " + hit.collider.gameObject.transform.position);
+    //         }
+    //         else
+    //         {
+    //             // hide script panel
+    //             ScriptManager.instance.ClearScriptable();
+    //             ScriptManager.instance.SetActivePanel(false);
+    //         }
+    //     }
+    // }
+
+    public void OnPointerDown(PointerEventData eventData)
     {
+        // hide script panel
         ScriptManager.instance.ClearScriptable();
         ScriptManager.instance.SetActivePanel(false);
-    }
-
-    void Update()
-    {
-        if (Input.GetMouseButtonDown(0))
-        {
-            RaycastHit2D hit = Physics2D.Raycast(Camera.main.ScreenToWorldPoint(Input.mousePosition), Vector2.zero);
-
-            if (hit.collider != null)
-            {
-                Debug.Log("Target Position: " + hit.collider.gameObject.transform.position);
-            }
-        }
     }
 }

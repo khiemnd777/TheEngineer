61b1417139f764e6928409cbe063f3febf7ac768
Khiemnd <khiemnd777@gmail.com>
2018-03-09 01:46:16 +0700

    texture maker v1

---
+++ Source/Assets/Scripts/TextureMaker/TextureMaker.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TextureMaker : MonoBehaviour 
+{
+	public RectTransform textureMakerRect;
+	public float textureWidth;
+	public float textureHeight;
+	public float spaceX;
+	public float spaceY;
+	public TextureTile tilePrefab;
+	public List<TextureTile> tiles;
+
+	RectTransform rectTransform;
+
+	// void Start()
+	// {
+	// 	var rectWidth = textureMakerRect.rect.width;
+	// 	var rectHeight = textureMakerRect.rect.height;
+	// 	// new Texture2D(textureWidth, textureHeight, TextureFormat.)
+	// 	// pushing tiles into array by dimension of texture
+	// 	tiles = new List<TextureTile>();
+	// 	var numOfTile = textureWidth * textureHeight;
+	// 	for(var i = 0; i < numOfTile; i++){
+	// 		var tile = Instantiate<TextureTile>(tilePrefab, Vector3.zero, Quaternion.identity);
+	// 		var tileRect = tile.GetComponent<RectTransform>();
+	// 		tileRect.sizeDelta = new Vector2(rectWidth / textureWidth, rectHeight / textureHeight);
+	// 		tiles.Add(tile);
+	// 		tile.transform.SetParent(textureMakerRect);
+	// 		tile.transform.localPosition = Vector3.zero;
+	// 		tileRect = null;
+	// 		tile = null;
+	// 	}
+	// 	// computing dimension of grid
+
+	// }
+}
---
+++ Source/Assets/TextureTest.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class TextureTest : MonoBehaviour
+{
+    MeshRenderer _renderer;
+    Texture2D tex;
+
+    // Use this for initialization
+    void Start()
+    {
+        _renderer = GetComponent<MeshRenderer>();
+        tex = new Texture2D(16, 16, TextureFormat.RGBA32, true);
+        for (var x = 0; x < tex.width; x++)
+        {
+            for (var y = 0; y < tex.height; y++)
+            {
+                var color = (x & y) != 0 ? Color.white : Color.grey;
+                tex.SetPixel(x, y, color);
+            }
+        }
+        tex.Apply();
+        tex.wrapMode = TextureWrapMode.Clamp;
+        tex.filterMode = FilterMode.Point;
+        // var sprite = Sprite.Create(tex, new Rect(0, 0, tex.width, tex.height), Vector2.one * .5f);
+        // _renderer.sprite = sprite;
+        var mainMaterial = _renderer.materials[0];
+        mainMaterial.mainTexture = tex;
+        mainMaterial.shader = Shader.Find("Sprites/Default");
+        // scale 
+        // transform.localScale = new Vector3(100f / tex.width, 100f / tex.height, 1);
+    }
+
+    void Update()
+    {
+        var hit = Physics2D.Raycast(Camera.main.ScreenToWorldPoint(Input.mousePosition), Vector2.zero);
+        Color c;
+        if (hit.collider != null)
+        {
+			var hitPointX = hit.point.x + 1/2f;
+			var hitPointY = hit.point.y + 1/2f;
+			var x = Mathf.Abs(transform.localPosition.x - hitPointX) / 1 * tex.width;
+			var y = Mathf.Abs(transform.localPosition.y - hitPointY)  / 1 * tex.height;
+            c = tex.GetPixel((int)x, (int)y); // Get color from texture
+			Debug.Log(c);
+        }
+
+    }
+}
